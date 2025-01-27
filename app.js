32a4244b461ab3e37879e77b2314e780e80c574f
Sufyan Chohan <chohansufyan69@gmail.com>
2025-01-27 12:48:43 +0500

    done

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ function calculation(){
     let receive =parseFloat (document.getElementById("inp2").value);
     console.log(purchase+receive);
     if (purchase < receive){
-        document.getElementById("para").innerText=`You Have RS ${receive-purchase} Profit`
+        document.getElementById("para").innerText=`You have RS ${receive-purchase} Profit`
+        document.getElementById("inp1").value=""
+        document.getElementById("inp2").value=""
 
     }
     else if (purchase > receive){
-        document.getElementById("para").innerText=`Oh No you have RS ${purchase-receive} Loss`
+        document.getElementById("para").innerText=`Oh no you have RS ${purchase-receive} Loss`
+         document.getElementById("inp1").value=""
+        document.getElementById("inp2").value=""
     }
     else if (purchase == receive){
-        document.getElementById("para").innerHTML=`No profit No Loss`
+        document.getElementById("para").innerHTML=`No Profit No Loss`
+         document.getElementById("inp1").value=""
+        document.getElementById("inp2").value=""
     }
     else{
         alert("Write Valid Value Here")
